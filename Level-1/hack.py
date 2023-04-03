2b1d6b7b8b936b1973a85753d98a2f79b591b802
fonglh <fonglh@users.noreply.github.com>
2023-04-04 02:47:58 +0800

    Use Decimal type for amount calculations in Level 1. (#14)
    * Use Decimal type for amount calculations in Level 1.
    
    This prevents other rounding bugs and comparison errors caused by
    the inability of floats to store base 10 numbers precisely.
    
    Add a test to hack.py to demonstrate the rounding error from using
    float.
    
    * improving the example
    
    * fixing styling issue
    
    ---------
    
    Co-authored-by: Joseph Katsioloudes <jkcso@github.com>

--- Level-1/hack.py
+++ Level-1/hack.py
@@ -11,5 +11,13 @@ class TestOnlineStore(unittest.TestCase):
         order_4 = c.Order(id='4', items=[payment, tv, reimbursement])
         self.assertEqual(c.validorder(order_4), 'Order ID: 4 - Payment imbalance: $-1000.00')
 
+    # Valid payments that should add up correctly, but do not
+    def test_5(self):
+        small_item = c.Item(type='product', description='accessory', amount=3.3, quantity=1)
+        payment_1 = c.Item(type='payment', description='invoice_5_1', amount=1.1, quantity=1)
+        payment_2 = c.Item(type='payment', description='invoice_5_2', amount=2.2, quantity=1)
+        order_5 = c.Order(id='5', items=[small_item, payment_1, payment_2])
+        self.assertEqual(c.validorder(order_5), 'Order ID: 5 - Full payment received!')
+
 if __name__ == '__main__':
     unittest.main()
