8ba0acf618dc23e088c0486533e3386c80b563a0
Joseph Katsioloudes <jkcso@github.com>
2023-04-14 20:07:56 +0100

    Update solution.py

--- Level-5/solution.py
+++ Level-5/solution.py
@@ -39,19 +39,24 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 PASSWORD_HASHER = 'SHA256_hasher'
 
 """
-Some mistakes are very basic, like choosing a cryptographically-broken algorithm
+Some mistakes are basic, like choosing a cryptographically-broken algorithm
 or committing secret keys directly in your source code.
 
 You are more likely to fall for something more advanced, like using functions that seem random
 but produce a weak randomness.
 
-Notice that I used the “random” module, which is designed for modeling and simulation, 
+The code suffers from:
+- reinventing the wheel by generating salt manually instead of calling gensalt()
+- not utilizing the full range of possible salt values
+- using the random module instead of the secrets module
+
+Notice that we used the “random” module, which is designed for modeling and simulation, 
 not for security or cryptography.
 
 A good practice is to use modules specifically designed and, most importantly,
 confirmed by the security community as secure for cryptography-related use cases.
 
-To fix the code, I used the “secrets” module, which provides access to the most secure source of 
+To fix the code, we used the “secrets” module, which provides access to the most secure source of 
 randomness on my operating system. I also used functions for generating secure tokens and hard-to-guess 
 URLs. 
 
