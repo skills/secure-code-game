afa03b1a5c04abab60f8238b43b607a9006a1062
Costa Paraskevopoulos <costa.paras1@gmail.com>
2023-08-15 13:27:27 +1000

    Simplify and neaten code

--- Level-1/code.py
+++ Level-1/code.py
@@ -25,9 +25,9 @@ def validorder(order: Order):
         elif item.type == 'product':
             net -= item.amount * item.quantity
         else:
-            return("Invalid item type: %s" % item.type)
+            return "Invalid item type: %s" % item.type
     
     if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
-        return("Order ID: %s - Full payment received!" % order.id)
+        return "Order ID: %s - Full payment received!" % order.id
--- Level-1/solution.py
+++ Level-1/solution.py
@@ -14,20 +14,20 @@ def validorder(order):
     for item in order.items:
         if item.type == 'payment':
             # sets a reasonable min & max value for the invoice amounts
-            if item.amount > -1*MAX_ITEM_AMOUNT and item.amount < MAX_ITEM_AMOUNT:
+            if -1 * MAX_ITEM_AMOUNT < item.amount < MAX_ITEM_AMOUNT:
                 net += Decimal(str(item.amount))
         elif item.type == 'product':
-            if item.quantity > 0 and item.quantity <= MAX_QUANTITY and item.amount > 0 and item.amount <= MAX_ITEM_AMOUNT:
+            if 0 < item.quantity <= MAX_QUANTITY and 0 < item.amount <= MAX_ITEM_AMOUNT:
                 net -= Decimal(str(item.amount)) * item.quantity
-            if net > MAX_TOTAL or net < -1*MAX_TOTAL:
-                return("Total amount exceeded")
+            if net > MAX_TOTAL or net < -1 * MAX_TOTAL:
+                return "Total amount exceeded"
         else:
-            return("Invalid item type: %s" % item.type)
+            return "Invalid item type: %s" % item.type
  
     if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
-        return("Order ID: %s - Full payment received!" % order.id)
+        return "Order ID: %s - Full payment received!" % order.id
 
 '''
 A floating-point underflow vulnerability.
