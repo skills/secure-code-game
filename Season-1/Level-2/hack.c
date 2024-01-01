aa7f7cea5048a1cd0097275986d725650dd23826
Ervin Varga <ervin.varga@gmail.com>
2024-01-01 14:09:38 +0000

    Fix issue #55

--- Season-1/Level-2/hack.c
+++ Season-1/Level-2/hack.c
@@ -20,19 +20,18 @@
 int main() {
     printf("Level 2 \n\n");
     // Creates a non-admin username called "pwned"
-    user_account* ua = create_user_account(false, "pwned");
-    printf("0. Non-admin (admin:%i) username called '%s' has been created \n", ua->isAdmin, ua->username);
+    int user1 = create_user_account(false, "pwned");
+    printf("0. Non-admin (admin:%i) username called '%s' has been created \n", is_admin(user1), username(user1));
     
     // An outsider or an insider managed to supply the following input that originally aimed to change a dummy non-admin setting.
-    update_setting(ua, "-7", "1");
+    update_setting(user1, "-7", "1");
     printf("1. A dummy setting has been set to dummy number '1' \n");
-    printf("2. Making sure user '%s' is not an admin by performing a check -> [Result] Admin:%i \n\n", ua->username, ua->isAdmin);
+    printf("2. Making sure user '%s' is not an admin by performing a check -> [Result] Admin:%i \n\n", username(user1), is_admin(user1));
     
-    if (ua->isAdmin == 1)
-        printf("<SOS> PRIVILEGE ESCALATION ATTACK DETECTED \n");
-    
-    if (ua->isAdmin == 0) 
-        printf("CONGRATULATIONS LEVEL 2 PASSED!");
+    if (is_admin(user1))
+        printf("<SOS> PRIVILEGE ESCALATION ATTACK DETECTED\n");
+    else
+        printf("CONGRATULATIONS LEVEL 2 PASSED!\n");
 
     return 0;
 }
--- Season-1/Level-2/tests.c
+++ Season-1/Level-2/tests.c
@@ -14,18 +14,18 @@
 int main() {
     printf("Level 2 \n\n");
     // Creates a non-admin username called "pwned"
-    user_account* ua = create_user_account(false, "pwned");
-    printf("0. Non-admin (admin:%i) username called '%s' has been created \n\n", ua->isAdmin, ua->username);
+    int user1 = create_user_account(false, "pwned");
+    printf("0. Non-admin (admin:%i) username called '%s' has been created \n\n", is_admin(user1), username(user1));
 
-    printf("1. Non-admin users like '%s' can update some dummy numerical settings \n", ua->username);
+    printf("1. Non-admin users like '%s' can update some dummy numerical settings \n", username(user1));
     printf("2. Non-admin users have no access to settings that can escalate themselves to admins \n\n"); 
 
     // Updates the setting '1' of the pwned username to the number '10'
-    update_setting(ua, "1", "10");
-    printf("3. Dummy setting '1' has been now set to dummy number '10' for user '%s' \n", ua->username);
-    printf("4. Making sure user '%s' is not an admin by performing a check -> [Result] Admin:%i \n\n", ua->username, ua->isAdmin);
+    update_setting(user1, "1", "10");
+    printf("3. Dummy setting '1' has been now set to dummy number '10' for user '%s' \n", username(user1));
+    printf("4. Making sure user '%s' is not an admin by performing a check -> [Result] Admin:%i \n\n", username(user1), is_admin(user1));
 
-    if (ua->isAdmin == 0) 
+    if (!is_admin(user1))
         printf("User is not an admin so the code works as expected... is it though? \n");
         
     return 0;
