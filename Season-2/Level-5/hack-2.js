bf963cfd7cdf61cb623e626152539a8b88d322bc
Joseph Katsioloudes <jkcso@github.com>
2024-04-04 20:56:17 +0100

    Apply suggestions from code review

--- Season-2/Level-5/hack-2.js
+++ Season-2/Level-5/hack-2.js
@@ -5,11 +5,12 @@
 // 1. Double click index.html to open it in any browser. Are you using GitHub Codespaces?
 
 // Please note that if you are inside a codespace, it is not possible to perform step 1.
-// Instead, run the following in terminal from the root of the game level (Season-2/Level-5/):
-//    `python3 -m http.server`
-// Once running, select on the "ports" tab of the codespace and click on the URL that exposes
-// port 8000. Navigating to the URL should serve the HTML and vulnerable JavaScript.
-// Proceed with the remaining steps.
+// Instead, run the following command inside the codespace's terminal:
+//    `cd Season-2/Level-5/ && python3 -m http.server`
+// A pop up window will appear on the bottom right informing you that 
+// "Your application running on port 8000 is available". Now click "Open in Browser".
+// Another way to open the application on port 8000 is by clicking on the "Ports" tab 
+// in terminal, followed by clicking on its respective URL.
 
 // 2. Copy the following line, paste it in the javascript console and press enter.
 CryptoAPI.sha1._round = function() { alert('Exploit 2'); };
