298fc1c56c967a53c4f7a474266eb4b7286ad0c1
Bartosz Galek <bartosz@galek.com.pl>
2025-03-18 19:04:37 +0100

    GITHUB_TOKEN name updated

--- Season-3/Level-1/code.js
+++ Season-3/Level-1/code.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({baseURL: "https://models.inference.ai.azure.com", apiKey: process.env.GH_TOKEN});
+const openai = new OpenAI({baseURL: "https://models.inference.ai.azure.com", apiKey: process.env.GITHUB_TOKEN});
 
 /**
  * The system message that the AI should not reveal the password to the user.
